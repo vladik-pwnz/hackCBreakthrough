6c81bee8a774fefe4a553de55fe7c317e7dccec3
Ronningen <step-lap@mail.ru>
2023-05-25 03:25:03 +0300

    yolov8 infrastructure

--- pathbook/pathbook.py
+++ pathbook/pathbook.py
@@ -1,6 +1,11 @@
-path_detector_onnx = r'model/bin/best.onnx'
+path_detector_onnx = r'/Users/samedi/Documents/Coding/swans/model/bin/best.onnx'
+
 path_initial_train_dataset = r'/Users/samedi/Desktop/Минприроды'
-path_train_dataset = r'data/train'
-path_val_dataset = r'data/val'
-path_test_dataset = r'data/test'
-path_submit = r'data/submit'
+path_train_dataset = r'/Users/samedi/Documents/Coding/swans/data/train'
+path_val_dataset = r'/Users/samedi/Documents/Coding/swans/data/val'
+path_test_dataset = r'/Users/samedi/Documents/Coding/swans/data/test'
+path_submit = r'/Users/samedi/Documents/Coding/swans/data/submit'
+
+path_test_annotation = r'/Users/samedi/Documents/Coding/swans/data/test_annotation.csv'
+path_train_annotation = r'/Users/samedi/Documents/Coding/swans/data/train_annotation.csv'
+path_val_annotation = r'/Users/samedi/Documents/Coding/swans/data/val_annotation.csv'
