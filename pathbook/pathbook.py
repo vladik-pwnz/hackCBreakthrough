9a29ad331853b8d37e1e832a22043eb15a4a4103
Ronningen <step-lap@mail.ru>
2023-05-25 07:23:10 +0300

    training

--- pathbook/pathbook.py
+++ pathbook/pathbook.py
@@ -1,4 +1,4 @@
-path_detector_onnx = r'/Users/samedi/Documents/Coding/swans/model/bin/best.onnx'
+path_detector_onnx = r'/Users/samedi/Documents/Coding/swans/model/bin/mike_yolov7.onnx'
 
 path_initial_train_dataset = r'/Users/samedi/Desktop/Минприроды'
 path_train_dataset = r'/Users/samedi/Documents/Coding/swans/data/train'
