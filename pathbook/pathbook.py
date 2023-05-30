aa286850db907731e5388d8751852861bdba67a6
Ronningen <step-lap@mail.ru>
2023-05-30 23:53:43 +0300

    models comparison, yaml

--- pathbook/pathbook.py
+++ pathbook/pathbook.py
@@ -2,12 +2,12 @@ path_detector_onnx = r'/Users/samedi/Documents/Coding/swans/model/bin/mike_yolov
 
 path_initial_train_dataset = r'/Users/samedi/Desktop/Минприроды'
 
-path_train_dataset = r'/Users/samedi/Documents/Coding/swans/data/train'
-path_val_dataset = r'/Users/samedi/Documents/Coding/swans/data/val'
-path_test_dataset = r'/Users/samedi/Documents/Coding/swans/data/test'
-path_cls_train_dataset = r'/Users/samedi/Documents/Coding/swans/data/classification/train'
-path_cls_val_dataset = r'/Users/samedi/Documents/Coding/swans/data/classification/val'
+path_train_dataset = r'/Users/samedi/Documents/Coding/swans/dataset/train'
+path_val_dataset = r'/Users/samedi/Documents/Coding/swans/dataset/val'
+path_test_dataset = r'/Users/samedi/Documents/Coding/swans/dataset/test'
+path_cls_train_dataset = r'/Users/samedi/Documents/Coding/swans/dataset/classification/train'
+path_cls_val_dataset = r'/Users/samedi/Documents/Coding/swans/dataset/classification/val'
 
-path_test_annotation = r'/Users/samedi/Documents/Coding/swans/data/test_annotation.csv'
-path_train_annotation = r'/Users/samedi/Documents/Coding/swans/data/train_annotation.csv'
-path_val_annotation = r'/Users/samedi/Documents/Coding/swans/data/val_annotation.csv'
+path_test_annotation = r'/Users/samedi/Documents/Coding/swans/dataset/test_annotation.csv'
+path_train_annotation = r'/Users/samedi/Documents/Coding/swans/dataset/train_annotation.csv'
+path_val_annotation = r'/Users/samedi/Documents/Coding/swans/dataset/val_annotation.csv'
